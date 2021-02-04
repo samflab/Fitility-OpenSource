952fb77b8b6df12af7ff877ea83efcb0e1cc7449
Niloy-Sikdar-001 <niloysikdar30@gmail.com>
2021-02-04 21:38:35 +0530

    Admin Home UI Implemented

--- fitility_admin/lib/main.dart
+++ fitility_admin/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:fitility_admin/screen/createpage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'screen/createpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CreatePage(),
     );
   }
 }
-
-
-
---
+++ fitility_admin/lib/screen/adminpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatefulWidget {
+  @override
+  _AdminPageState createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Center(
+        child: Text("Admin page"),
+      ),
+    );
+  }
+}
