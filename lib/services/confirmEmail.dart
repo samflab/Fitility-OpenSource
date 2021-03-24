421a075812ed12ac978e083a4697c9b21696a256
samflab <kasturimeh@gmail.com>
2021-03-24 19:33:59 +0530

    verify email

--- lib/services/confirmEmail.dart
+++ lib/services/confirmEmail.dart
@@ -4,14 +4,17 @@ class ConfirmEmail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text(
-            "Please confirm your email with the link sent on your email ID\ntry signing in again later.",
-            style: TextStyle(
-              fontFamily: 'Rubik',
-              fontSize: 16,
-              color: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.all(28.0),
+        child: Container(
+          child: Center(
+            child: Text(
+              "Please confirm your email with the link sent on your email ID\ntry signing in again later.",
+              style: TextStyle(
+                fontFamily: 'Rubik',
+                fontSize: 16,
+                color: Colors.black,
+              ),
             ),
           ),
         ),
